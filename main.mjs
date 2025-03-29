7fcf6b8c6e120199ad3ef33144af7462005a1c05
Fernando Cañas <fernando.canas@outlook.com>
2025-03-29 16:08:20 -0500

    feat: get phone number and return contacts sorted by name

--- main.mjs
+++ main.mjs
@@ -11,42 +11,55 @@ class AddressBook {
     this._book = {};
   }
 
-  addName(name) {
+  async go() {
+    console.log("Hello, welcome to your Address Book App (ABA)");
+
+    let name = await this.getName();
+    while (name !== "exit") {
+      const phone = await this.getPhone(name);
+      this.addContact(name, phone);
+
+      name = await this.getName();
+    }
+
+    this.displayBook();
+  }
+
+  addContact(name, phone) {
     if (!this._book[name]) {
-      this._book[name] = name;
+      this._book[name] = phone;
     }
   }
 
-  getNames() {
+  getAllContacts() {
     return this._book;
+  }
+
+  async getName() {
+    const rl = readline.createInterface({ input, output });
+    const name = await rl.question("Enter contact name ('exit' to quit): ");
+    rl.close();
+    return name;
+  }
+
+  async getPhone(name) {
+    const rl = readline.createInterface({ input, output });
+    const phone = await rl.question(`Enter phone number for ${name}: `);
+    rl.close();
+    return phone;
   }
 
   displayBook() {
     console.log("\nTEST: Display contents of address book");
     console.log("\nTEST: Address book contains the following contacts\n");
 
-    for (let item of Object.keys(this.getNames())) {
-      console.log(item);
+    const book = this.getAllContacts();
+    const names = Object.keys(book);
+    names.sort();
+
+    for (let name of names) {
+      console.log(`${name}: ${this._book[name]}`);
     }
-  }
-
-  async getName() {
-    const rl = readline.createInterface({ input, output });
-    const answer = await rl.question("Enter contact name ('exit' to quit): ");
-    rl.close();
-    return answer;
-  }
-
-  async go() {
-    console.log("Hello, welcome to your Address Book App (ABA)");
-
-    let name = await this.getName();
-    while (name !== "exit") {
-      this.addName(name);
-      name = await this.getName();
-    }
-
-    this.displayBook();
   }
 }
 
