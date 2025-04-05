de76ee51a1b8e8ef7d926411351dc386a18be1ba
Fernando Cañas <fernando.canas@outlook.com>
2025-04-05 10:36:44 -0500

    feat: get email and phone with input validation

--- main.mjs
+++ main.mjs
@@ -14,20 +14,21 @@ class AddressBook {
   async go() {
     console.log("Hello, welcome to your Address Book App (ABA)");
 
-    let name = await this.getName();
+    let name = await this.getName("Enter contact name ('exit' to quit): ");
     while (name !== "exit") {
-      const phone = await this.getPhone(name);
-      this.addContact(name, phone);
+      const phone = await this.getPhone(`Enter phone number for ${name}: `);
+      const email = await this.getAnswer(`Enter email for ${name}: `);
+      this.addContact(name, phone, email);
 
-      name = await this.getName();
+      name = await this.getName("Enter contact name ('exit' to quit): ");
     }
 
     this.displayBook();
   }
 
-  addContact(name, phone) {
+  addContact(name, phone, email) {
     if (!this._book[name]) {
-      this._book[name] = phone;
+      this._book[name] = { phone, email };
     }
   }
 
@@ -35,17 +36,62 @@ class AddressBook {
     return this._book;
   }
 
-  async getName() {
+  async getAnswer(prompt) {
     const rl = readline.createInterface({ input, output });
-    const name = await rl.question("Enter contact name ('exit' to quit): ");
+    const answer = await rl.question(prompt);
     rl.close();
+    return answer;
+  }
+
+  async getName(prompt) {
+    let name = "";
+    let again = true;
+
+    while (again) {
+      const regex = new RegExp("^[a-zA-Z ]*$");
+      name = await this.getAnswer(prompt);
+
+      if (name.length === 0) {
+        console.error(
+          "A contact name must contain at least one uppercase or lowercase letter"
+        );
+        continue;
+      }
+
+      if (!regex.test(name)) {
+        console.error(
+          "A contact name must contain only uppercase or lowercase letters and spaces"
+        );
+        continue;
+      }
+
+      again = false;
+    }
+
     return name;
   }
 
-  async getPhone(name) {
-    const rl = readline.createInterface({ input, output });
-    const phone = await rl.question(`Enter phone number for ${name}: `);
-    rl.close();
+  async getPhone(prompt) {
+    let phone = "";
+    let again = true;
+
+    while (again) {
+      const regex = new RegExp("^[0-9]*$");
+      phone = await this.getAnswer(prompt);
+
+      if (phone.length === 0) {
+        console.error("A contact phone must contain at least one number");
+        continue;
+      }
+
+      if (!regex.test(phone)) {
+        console.error("A contact phone must contain only numbers");
+        continue;
+      }
+
+      again = false;
+    }
+
     return phone;
   }
 
@@ -58,7 +104,9 @@ class AddressBook {
     names.sort();
 
     for (let name of names) {
-      console.log(`${name}: ${this._book[name]}`);
+      console.log(`${name}: 
+        phone: ${this._book[name].phone}
+        email: ${this._book[name].email}`);
     }
   }
 }
